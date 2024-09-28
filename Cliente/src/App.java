c76c90f45143de8b9b945f97874ce813a032aa80
Sergew_RMZ <serge15games@gmail.com>
2024-09-28 17:59:11 -0600

    Carpeta inncesaria eliminada

--- Cliente/src/App.java
+++
@@ -1,8 +0,0 @@
-import modules.ClientServer;
-
-public class App {
-    public static void main (String[] args) {
-        ClientServer client = new ClientServer();
-        client.sendMessage("Hola Denia");
-    }
-}
